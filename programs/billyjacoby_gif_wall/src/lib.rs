017d25af919c62129a86da7a7537efe9b4009952
Billy Jacoby <billyjacoby@gmail.com>
2021-11-09 17:04:26 -0500

    initial commit

---
+++ programs/billyjacoby_gif_wall/src/lib.rs
@@ -0,0 +1,65 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod billyjacoby_gif_wall {
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+        // Get a reference to the account
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String, user_name: String) -> ProgramResult {
+        // Get a reference to the account and increment the total gifs
+        let base_account = &mut ctx.accounts.base_account;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *base_account.to_account_info().key,
+            user_name: user_name.to_string(),
+        };
+
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
+}
+
+// Attach certain variables to the StartStuffOff context
+#[derive(Accounts)]
+pub struct StartStuffOff<'info>{
+    #[account(init, payer=user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+
+
+// Create a custom struct to work with
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+    pub user_name: String,
+}
+
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a vector of type itemSTruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
